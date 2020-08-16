8aa62740dd3cc409f57680d5c482e60f558bab5b
Trusty Coder <trustycoder83@gmail.com>
2020-08-17 00:45:17 +0300

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# lunar-lander-v2
+# LunarLander-v2
 Landing pad is always at coordinates (0,0). Coordinates are the first two numbers in state vector. Reward for moving from the top of the screen to landing pad and zero speed is about 100..140 points. If lander moves away from landing pad it loses reward back. Episode finishes if the lander crashes or comes to rest, receiving additional -100 or +100 points. Each leg ground contact is +10. Firing main engine is -0.3 points each frame. Solved is 200 points. Landing outside landing pad is possible. Fuel is infinite, so an agent can learn to fly and then land on its first attempt. Four discrete actions available: do nothing, fire left orientation engine, fire main engine, fire right orientation engine.
+
+## OpenAI Gym
+OpenAI Gym is a toolkit for developing and comparing reinforcement learning algorithms. It supports teaching agents everything from walking to playing games like pong or pinball. Gym is an open source interface to reinforcement learning tasks.
+
+## Requirements
+- [Python 3.6 or 3.7](https://www.python.org/downloads/release/python-360/)
+- [CUDA Toolkit 10.1](https://developer.nvidia.com/cuda-10.1-download-archive-base)
+- [cuDNN v7.6.5](https://developer.nvidia.com/cuda-10.1-download-archive-base)
+- [Pipenv](https://pypi.org/project/pipenv/)
+- [Visual C++ 2015 Build Tools](http://go.microsoft.com/fwlink/?LinkId=691126&fixForIE=.exe.)
+
+## How to install the packages
+You can install the required Python packages using the following command:
+- `pipenv sync`
+
+## Deep Q-learning with Experience Replay
+A deep Q network (DQN) (Mnih et al., 2013) is an extension of Q learning, which is a typical deep reinforcement learning method. In DQN, a Q function expresses all action values under all states, and it is approximated using a convolutional neural network. Using the approximated Q function, an optimal policy can be derived. In DQN, a target network, which calculates a target value and is updated by the Q function at regular intervals, is introduced to stabilize the learning process. In DQN, learning is stabilized through a heuristic called experience replay (Lin, 1993) and the use of a target network. Experience replay is a technique that saves time-series data in a buffer called replay memory.
+
+## How to run it
+You can run the script using the following command: 
+- `pipenv run python dqn_lunar_lander.py`
+
+## Improvement ideas
+- improve the code quality
+- remove unnecessary comments
---
+++ dqn_lunar_lander.py
@@ -0,0 +1,128 @@
+"""
+LunarLander-v2 -- Deep Q-learning with Experience Replay
+"""
+import random
+from collections import deque
+
+import gym
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+
+class Brain:
+    def __init__(self, state_size, action_size, memory_size=50000):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=memory_size)
+        self.gamma = 0.95  # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def memorize(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if random.uniform(0, 1) < self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            return np.argmax(self.model.predict(state)[0])
+
+    def replay(self, batch_size=32):
+        for state, action, reward, next_state, done in random.sample(self.memory, batch_size):
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+if __name__ == "__main__":
+    # Flag used to enable or disable screen recording
+    recording_is_enabled = False
+
+    # Initializes the environment
+    env = gym.make('LunarLander-v2')
+
+    # Records the environment
+    if recording_is_enabled:
+        env = gym.wrappers.Monitor(env, "recording", video_callable=lambda episode_id: True, force=True)
+
+    # Defines training related constants
+    batch_size = 32
+    num_episodes = 5000
+    num_episode_steps = 10000  # constant value
+    action_size = env.action_space.n
+    state_size = env.observation_space.shape[0]
+
+    # Creates the brain
+    brain = Brain(state_size=state_size, action_size=action_size)
+
+    for episode in range(num_episodes):
+        # Defines the total reward per episode
+        total_reward = 0
+
+        # Resets the environment
+        observation = env.reset()
+
+        # Gets the state
+        state = np.reshape(observation, [1, state_size])
+
+        for episode_step in range(num_episode_steps):
+            # Renders the screen after new environment observation
+            env.render(mode="human")
+
+            # Gets a new action
+            action = brain.act(state)
+
+            # Takes action and calculates the total reward
+            observation, reward, done, _ = env.step(action)
+            total_reward += reward
+
+            # Gets the next state
+            next_state = np.reshape(observation, [1, state_size])
+
+            # Memorizes the experience
+            brain.memorize(state, action, reward, next_state, done)
+
+            # Updates the state
+            state = next_state
+
+            if done:
+                print("Episode %d/%d finished after %d episode steps with total reward = %f."
+                      % (episode + 1, num_episodes, episode_step + 1, total_reward))
+                break
+
+            elif episode_step >= num_episode_steps - 1:
+                print("Episode %d/%d timed out at %d with total reward = %f."
+                      % (episode + 1, num_episodes, episode_step + 1, total_reward))
+
+            if len(brain.memory) > batch_size:
+                brain.replay(batch_size)
+
+        # Stores memory on disk
+        brain.save("lunar_lander.h5")
+
+    # Closes the environment
+    env.close()
